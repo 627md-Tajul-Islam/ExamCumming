9fb9b5a30e836789c57bbdd3c6c89e7e290ba68c
627md-Tajul-Islam <tajul.azazil@gmail.com>
2023-01-12 20:49:30 +0600

    ok

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const endDate = "1 February 2023 2:30:00 PM"
+const endDate = "2 February 2023 10:00:00 AM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
